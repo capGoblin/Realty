cda3b7308350ea9567b10752e24f1a039a930b31
capGoblin <dharshan2457@gmail.com>
2024-07-07 19:46:03 +0530

    test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
+function Hello(samer: string) {
+  console.log(samer);
+}
+
+Hello("Samer");
+
+Hello("Samer");
+
 export default function Home() {
   return (
     <div className={styles.page}>
